50b23c45c317687fd6501d04800816df0c45423d
robisonwebdev <david@robisonweb.dev>
2021-09-10 14:12:11 -0600

    Imported Header.css. Added 3 temp divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styles/Header.css';
 
 const Header = () => {
     return (
-        <div>HEADER</div>
+        <div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </div>
     );
 }
 
