f8d03c24b82a02992865be02a3f499776ca3bc12
robisonwebdev <david@robisonweb.dev>
2021-09-11 09:13:32 -0600

    Imported ControlBoard.js Added ControlBoard to main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Gameboard from './Gameboard';
+import ControlBoard from './ControlBoard';
 
 const Main = () => {
     return (
         <main>
             <h1>Tic-Tac-Toe</h1>
             <Gameboard />
+            <ControlBoard />
         </main>
     );
 }
