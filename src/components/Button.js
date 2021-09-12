ccab1c918ab284eb5132c025b166e019b06d8b6b
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:25:26 -0600

    Destructured props. Setup type and onClick to accept props, and added text prop for button text.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ onClick, text, type }) => {
     return (
-        <button>Button</button>
+        <button type={type} onClick={onClick}>{text}</button>
     );
 }
 
