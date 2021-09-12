955daee3a0a605a8f346b04c27fc42eaa31ba371
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:26:13 -0600

    Passed props to Button component.

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -9,7 +9,7 @@ const ControlBoard = () => {
             <form>
                 <FormInput className='playerLabel' labelText='Player X' placeholder='Enter Player 1' type='text' />
                 <FormInput className='playerLabel' labelText='Player O' placeholder='Enter Player 2' type='text' />
-                <Button />
+                <Button text='Start!' type='submit' />
             </form>
         </div>
     );
