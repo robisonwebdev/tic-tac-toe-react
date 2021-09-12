7bbd848e5c819997cbaadb32263b90fceb590c3c
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:15:22 -0600

    Gave div a className of controlBoard. Added form inside .controlBoard. Added FormInput's to form with temp data.

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import FormInput from './FormInput';
 
 const ControlBoard = () => {
     return (
-        <div>ControlBoard</div>
+        <div className='controlBoard'>
+            <form>
+                <FormInput labelText='Player X' placeholder='Enter Player 1' type='text' />
+                <FormInput labelText='Player O' placeholder='Enter Player 2' type='text' />
+            </form>
+        </div>
     );
 }
 
