1b2897a61184c1544cc2691947391e1cfbad7051
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:23:42 -0600

    Imported Button component. Added Button component to .controlBoard form

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from './FormInput';
+import Button from './Button';
 import '../styles/ControlBoard.css';
 
 const ControlBoard = () => {
@@ -8,6 +9,7 @@ const ControlBoard = () => {
             <form>
                 <FormInput className='playerLabel' labelText='Player X' placeholder='Enter Player 1' type='text' />
                 <FormInput className='playerLabel' labelText='Player O' placeholder='Enter Player 2' type='text' />
+                <Button />
             </form>
         </div>
     );
