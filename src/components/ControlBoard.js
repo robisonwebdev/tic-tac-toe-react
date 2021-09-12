6d4194ca763412de34e52f566dcbaf4f3779ee2e
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:28:50 -0600

    Setup a handleSubmit function to prevent default on form submit. Passed handleSubmit to onSubmit in form

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -4,9 +4,14 @@ import Button from './Button';
 import '../styles/ControlBoard.css';
 
 const ControlBoard = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submit Clicked!');
+    }
+
     return (
         <div className='controlBoard'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FormInput className='playerLabel' labelText='Player X' placeholder='Enter Player 1' type='text' />
                 <FormInput className='playerLabel' labelText='Player O' placeholder='Enter Player 2' type='text' />
                 <Button text='Start!' type='submit' />
