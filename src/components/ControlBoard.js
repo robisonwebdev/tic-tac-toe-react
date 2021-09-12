c26857ef51820fd1fb7f4d99c239b8aa7d843f4c
robisonwebdev <david@robisonweb.dev>
2021-09-12 13:22:39 -0600

    Passed className props to both FormInput components.

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -6,8 +6,8 @@ const ControlBoard = () => {
     return (
         <div className='controlBoard'>
             <form>
-                <FormInput labelText='Player X' placeholder='Enter Player 1' type='text' />
-                <FormInput labelText='Player O' placeholder='Enter Player 2' type='text' />
+                <FormInput className='playerLabel' labelText='Player X' placeholder='Enter Player 1' type='text' />
+                <FormInput className='playerLabel' labelText='Player O' placeholder='Enter Player 2' type='text' />
             </form>
         </div>
     );
