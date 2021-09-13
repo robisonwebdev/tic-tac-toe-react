7f5b3ddf0b80dd25e1aea0719caf05e10252be85
robisonwebdev <david@robisonweb.dev>
2021-09-13 12:27:55 -0600

    Destructured props. Added onChange and value props to FormInput component's and passed down player props to them.

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -3,7 +3,7 @@ import FormInput from './FormInput';
 import Button from './Button';
 import '../styles/ControlBoard.css';
 
-const ControlBoard = () => {
+const ControlBoard = ({ playerOne, playerTwo, setPlayerOne, setPlayerTwo }) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('Submit Clicked!');
@@ -12,8 +12,22 @@ const ControlBoard = () => {
     return (
         <div className='controlBoard'>
             <form onSubmit={handleSubmit}>
-                <FormInput className='playerLabel' labelText='Player X' placeholder='Enter Player 1' type='text' />
-                <FormInput className='playerLabel' labelText='Player O' placeholder='Enter Player 2' type='text' />
+                <FormInput
+                    className='playerLabel'
+                    labelText='Player X'
+                    onChange={setPlayerOne}
+                    placeholder='Enter Player 1'
+                    type='text'
+                    value={playerOne}
+                />
+                <FormInput
+                    className='playerLabel'
+                    labelText='Player O'
+                    onChange={setPlayerTwo}
+                    placeholder='Enter Player 2'
+                    type='text'
+                    value={playerTwo}
+                />
                 <Button text='Start!' type='submit' />
             </form>
         </div>
