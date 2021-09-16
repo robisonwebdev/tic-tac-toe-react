2851ee94b1c59d737c2287c32b7ed1000b741f40
robisonwebdev <david@robisonweb.dev>
2021-09-16 09:56:18 -0600

    Imported squareData. Setup useState for boardData and set the inital value to squareData. Created function called placeMarker to change the value(temp) of square, and update baordData. Created buildSquare variable that maps over boardData and return GameSquare component. Called buildSquare in gameboard div.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameSquare from './GameSquare';
+import squareData from '../squareData';
 import '../styles/Gameboard.css';
 
 const Gameboard = () => {
+    const [boardData, setBoardData] = useState(squareData);
+
+    const placeMarker = (id) => {
+        let updateValue = boardData.map(square => {
+            if (square.id === id) {
+                return {...square, value: 'B'};
+            }
+            return square;
+        })
+
+        setBoardData(updateValue);
+    }
+
+    const buildSquares = boardData.map(square => {
+        return <GameSquare key={square.id} onClick={placeMarker} id={square.id} value={square.value} />
+    })
+
     return (
         <div className='gameboard'>
-            <GameSquare />
+            {buildSquares}
         </div>
     );
 }
