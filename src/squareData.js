f95e2fd5df3f1edc4c5b6a96e4e34e8ba92bf15d
robisonwebdev <david@robisonweb.dev>
2021-09-16 09:15:26 -0600

    Setup an array of objects called squareData. Have a total of nine objects, each containing number value and a value value

---
+++ src/squareData.js
@@ -0,0 +1,40 @@
+let squareData = [
+    {
+        number: 1,
+        value: null
+    },
+    {
+        number: 2,
+        value: null
+    },
+    {
+        number: 3,
+        value: null
+    },
+    {
+        number: 4,
+        value: null
+    },
+    {
+        number: 5,
+        value: null
+    },
+    {
+        number: 6,
+        value: null
+    },
+    {
+        number: 7,
+        value: null
+    },
+    {
+        number: 8,
+        value: null
+    },
+    {
+        number: 9,
+        value: null
+    }
+];
+
+export default squareData;
