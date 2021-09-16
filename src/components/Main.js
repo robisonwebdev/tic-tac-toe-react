e7e8afbf873d558130ff0ed95b8af7e8fbacd82e
robisonwebdev <david@robisonweb.dev>
2021-09-16 10:54:47 -0600

    Setup useState for startGame, with an inital value of false. Passed startGame to Gameboard component. Passed setStartGame to ControlBoard component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Gameboard from './Gameboard';
 import ControlBoard from './ControlBoard';
 import '../styles/Main.css';
 
 const Main = ({ playerOne, playerTwo, setPlayerOne, setPlayerTwo }) => {
+    const [startGame, setStartGame] = useState(false);
+
     return (
         <main>
             <h1>Tic-Tac-Toe</h1>
-            <Gameboard />
+            <Gameboard
+                startGame={startGame}
+            />
             <ControlBoard
                 playerOne={playerOne}
                 playerTwo={playerTwo}
                 setPlayerOne={setPlayerOne}
                 setPlayerTwo={setPlayerTwo}
+                setStartGame={setStartGame}
             />
         </main>
     );
