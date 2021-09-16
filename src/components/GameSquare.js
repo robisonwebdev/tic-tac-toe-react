f6eec3ab65288051c3c0d45314e7c2ff0d5a5260
robisonwebdev <david@robisonweb.dev>
2021-09-16 09:58:33 -0600

    Added onClick and id props. Setup onClick event on div, that calls onClick prop and passing the value of id. Added squareID to div and passed it value of id.

--- src/components/GameSquare.js
+++ src/components/GameSquare.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/GameSquare.css';
 
-const GameSquare = ({ squareValue }) => {
+const GameSquare = ({ onClick, id, value }) => {
     return (
-        <div className='gameSquare'>
-            {squareValue}
+        <div className='gameSquare' onClick={() => onClick(id)} squareID={id}>
+            {value}
         </div>
     );
 }
