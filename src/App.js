ad37c03ba840c1aadf8219aad2d1514b5fb3db0e
robisonwebdev <david@robisonweb.dev>
2021-09-18 06:15:44 -0600

    Passed currentPlayer down to Header component.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   return (
     <div className='app'>
       <Header
+        currentPlayer={currentPlayer}
         playerOneScore={playerOneScore}
         playerTwoScore={playerTwoScore}
       />
