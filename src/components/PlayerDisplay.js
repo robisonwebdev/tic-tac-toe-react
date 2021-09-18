26f2dc9fdaf171b3c0729abd17256e68b0a99aae
robisonwebdev <david@robisonweb.dev>
2021-09-18 07:13:37 -0600

    Destructured position. Added position to div className

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/PlayerDisplay.css';
 
-const PlayerDisplay = ({ currentPlayer, player }) => {
+const PlayerDisplay = ({ currentPlayer, player, position }) => {
     return (
-        <div className={`playerDisplay ${currentPlayer ? 'highlight' : ''}`}>
+        <div className={`playerDisplay ${currentPlayer ? 'highlight' : ''} ${position}`}>
             <h1>{player}</h1>
         </div>
     );
