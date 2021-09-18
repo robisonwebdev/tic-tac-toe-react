2981fcbf50d38a12dc75612094fd7b4bc92e8bb0
robisonwebdev <david@robisonweb.dev>
2021-09-18 07:25:50 -0600

    Changed the value of the truth side on the ternary operator.

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -3,7 +3,7 @@ import '../styles/PlayerDisplay.css';
 
 const PlayerDisplay = ({ currentPlayer, player, position }) => {
     return (
-        <div className={`playerDisplay ${currentPlayer ? 'highlight' : ''} ${position}`}>
+        <div className={`playerDisplay ${currentPlayer ? `${position}Highlight` : ''} ${position}`}>
             <h1>{player}</h1>
         </div>
     );
